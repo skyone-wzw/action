d178a49f3f211ecdea368ccfda00fa52e9f23546
skyone-wzw <2654431601@qq.com>
2021-04-18 14:20:43 +0800

    [2021-04-18:6] 暂时修复因后缀导致的404错误，待优化

--- action/pipelines.py
+++ action/pipelines.py
@@ -6,6 +6,10 @@ class DownloadPipeline:
     def process_item(self, item, spider):
         name = str(item["name"])
         res = requests.get(item["url"], headers=item["headers"])
+        if "404" in res.text:
+            item["name"] = item["name"][:-3] + "png"
+            item["url"] = item["url"][:-3] + "png"
+            res = requests.get(item["url"], headers=item["headers"])
         with open(path.join(path.join(path.abspath('.'), item["path"]), name), 'wb') as file:
             file.write(res.content)
             print(item["title"])
